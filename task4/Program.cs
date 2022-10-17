91a3ca97255f138ed2f07050ec19a8482166f272
Sergey.Istomin <Sergej.istomin.2017@inbox.ru>
2022-10-17 22:41:36 +0700

    Add task4 and done

---
+++ task4/Program.cs
@@ -0,0 +1,52 @@
+﻿// Task 4
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int a1 = 2;
+int b1 = 3;
+int c1 = 7;
+
+Console.WriteLine("Number a = " + a1);
+Console.WriteLine("Number b = " + b1);
+Console.WriteLine("Number c = " + c1);
+
+int max1 = a1;
+
+if (a1 > max1) max1 = a1;
+if (b1 > max1) max1 = b1;
+if (c1 > max1) max1 = c1;
+
+Console.WriteLine("Max = " + max1);
+Console.WriteLine("===================");
+
+int a2 = 44;
+int b2 = 5;
+int c2 = 78;
+
+Console.WriteLine("Number a = " + a2);
+Console.WriteLine("Number b = " + b2);
+Console.WriteLine("Number c = " + c2);
+
+int max2 = a2;
+
+if (a2 > max2) max2 = a2;
+if (b2 > max2) max2 = b2;
+if (c2 > max2) max2 = c2;
+
+Console.WriteLine("Max = " + max2);
+Console.WriteLine("===================");
+
+int a3 = 22;
+int b3 = 3;
+int c3 = 9;
+
+Console.WriteLine("Number a = " + a3);
+Console.WriteLine("Number b = " + b3);
+Console.WriteLine("Number c = " + c3);
+
+int max3 = a3;
+
+if (a3 > max3) max3 = a3;
+if (b3 > max3) max3 = b3;
+if (c3 > max3) max3 = c3;
+
+Console.WriteLine("Max = " + max3);
+Console.WriteLine("===================");
